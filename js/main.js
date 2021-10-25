a07dbaff96392ed4c6bc30425641399a116c99ce
Grey <lserg3@yandex.ru>
2021-10-25 13:52:59 +0300

    отправка решения задачи 4.9

--- js/main.js
+++ js/main.js
@@ -6,15 +6,93 @@ const getRandomIntenger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 getRandomIntenger(12, 25);
-/*Функция, возвращающая случайное число с плавающей точкой из переданного диапазона
-включительно. Будет использоваться для генерации временных географических координат
-в следующем задании.*/
-const getRandomCoordinates = (min, max, precision) => {
-  if (min < 0 || max < 0 || min >= max) {
-    // eslint-disable-next-line quotes
-    return 'Ошибка! Введенное число меньше нуля, либо числа равны друг другу!';
+
+function getRandomPositiveFloat(a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+}
+
+const TITLE = [
+  'У оленя',
+  'В госях у Красной Шапочки',
+  'Дом трех поросят',
+  'E-flat',
+  'Васюкинские зори',
+  'Hiroto apartment',
+  'Lastel',
+  'Love Hotel',
+  'bungalow sushi',
+  'Shinjuku Gyo-en',
+];
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const CHECKOUT = ['12:00', '13:00', '14:00'];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION = [
+  'Комната в трёхкомнатной квартире, подойдёт молодым путешественникам.',
+  'У нас тут все ништяк. Ларек за углом. Шава 24 часа. Приезжайте! Интернетов нет!',
+  'Хейтеров просьба не беспокоить.',
+  'Великолепная лавочка прямо в центре парка. Подходит для всех кто любит спать на свежем воздухе.',
+  'Замечательный дворец в старинном центре города. Только для тех кто может себе позволить дворец. Лакеев и прочих жокеев просим не беспокоить.',
+  'Маленькая чистая квратира на краю парка. Без интернета, регистрации и СМС.',
+  'Квартира на первом этаже. Соседи тихие. Для всех, кто терпеть не может шума и суеты.',
+  'Великолепная квартира-студия в центре Токио. Подходит как туристам, там и бизнесменам. Квартира полностью укомплектована и имеет свежий ремонт.',
+  'Маленькая квартирка на чердаке. Для самых не требовательных.',
+  'Великолепная квартира-студия в центре Токио. Подходит как туристам, там и бизнесменам. Квартира полностью укомплектована и имеет свежий ремонт.',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const AD_COUNT = 10;
+const getRandomArrayElement = (elements) => elements[getRandomIntenger(0, elements.length - 1)];
+const getRandomArray = (arr) => {
+  const randomArray = [];
+  for (let i = 0; i < getRandomIntenger(0, arr.length); i++) {
+    randomArray.push(arr[i]);
   }
-  const randomNumber = Math.random() * (max - min + 1) + min;
-  return randomNumber.toFixed(precision);
+  return randomArray;
 };
-getRandomCoordinates(1, 10, 3);
+
+const createAd = () => {
+  const AUTHOR = new Object();
+  AUTHOR.avatar = `img/avatars/user0${getRandomIntenger(1, 10)}.png`; //Адреса
+  const OFFER = new Object();
+  OFFER.title = getRandomArrayElement(TITLE);
+  OFFER.adress = `${(location.lat = getRandomPositiveFloat(
+    35.65,
+    35.7,
+    5,
+  ))}, ${(location.lng = getRandomPositiveFloat(139.7, 139.8, 5))}`;
+  OFFER.price = getRandomIntenger(100, 2000);
+  OFFER.type = getRandomArrayElement(TYPE);
+  OFFER.rooms = getRandomIntenger(1, 6);
+  OFFER.guests = getRandomIntenger(1, 7);
+  OFFER.checkin = getRandomArrayElement(CHECKIN);
+  OFFER.checkout = getRandomArrayElement(CHECKOUT);
+  OFFER.features = getRandomArray(FEATURES);
+  OFFER.description = getRandomArrayElement(DESCRIPTION);
+  OFFER.photos = getRandomArray(PHOTOS);
+  const LOCATION = new Object();
+  LOCATION.lat = location.lat;
+  LOCATION.lng = location.lng;
+
+  return {
+    AUTHOR,
+    OFFER,
+    LOCATION,
+  };
+};
+const similaAd = Array.from({ length: AD_COUNT }, createAd);
+

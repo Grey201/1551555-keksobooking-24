28c3e6e478ccc8c9048a5e800c4634f299d7807e
Grey <lserg3@yandex.ru>
2021-11-09 22:46:32 +0300

    3-е исправление ошибок задача 10.6

--- js/card.js
+++ js/card.js
@@ -1,7 +1,13 @@
-import {createAds} from './data.js';
+import { createAds } from './data.js';
+
+const CENTER_POINT_LAT = 35.6895;
+const CENTER_POINT_LNG = 139.692;
+const ZOOM_LEVEL = 10;
+const ICON_SIZE = [36, 36];
+const ICON_ANCHOR = [18, 36];
 
 const similarAds = createAds();
-const address=document.querySelector('#address');
+const address = document.querySelector('#address');
 
 const getSimilarAd = (ad) => {
   const cardTemplate = document
@@ -9,25 +15,23 @@ const getSimilarAd = (ad) => {
     .content.querySelector('.popup');
   const adElement = cardTemplate.cloneNode(true);
   adElement.querySelector('.popup__title').textContent = ad.offer.title;
-  adElement.querySelector('.popup__text--address').textContent =
-    ad.offer.address;
-  adElement.querySelector(
-    '.popup__text--price',
-  ).textContent = `${ad.offer.price} ₽/ночь`;
-  adElement.querySelector(
-    '.popup__description').textContent = ad.offer.description;
+  adElement.querySelector('.popup__text--address').textContent = ad.offer.address;
+  adElement.querySelector('.popup__text--price').textContent = `${ad.offer.price} ₽/ночь`;
+  adElement.querySelector('.popup__description').textContent =
+    ad.offer.description;
   adElement.querySelector('.popup__type').textContent = ad.offer.type;
-  adElement.querySelector(
-    '.popup__text--capacity',
-  ).textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
-  adElement.querySelector(
-    '.popup__text--time',
-  ).textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
+  adElement.querySelector('.popup__text--capacity').textContent = (
+    `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`
+  );
+  adElement.querySelector('.popup__text--time').textContent = (
+    `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`
+  );
   const featureContainer = adElement.querySelector('.popup__features');
   const featureList = featureContainer.querySelectorAll('.popup__feature');
   const collectImg = adElement.querySelector('.popup__photos');
   const img = collectImg.querySelector('img');
   const modifiers = ad.offer.features.map((feature) => `popup__feature--${feature}`);
+
   featureList.forEach((featureListItems) => {
     const modifier = featureListItems.classList[1];
 
@@ -48,28 +52,29 @@ const getSimilarAd = (ad) => {
   return adElement;
 };
 
-const map=L.map('map-canvas').setView({
-  lat: 35.6895,
-  lng: 139.692,
-}, 10);
-
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+const map = L.map('map-canvas').setView(
   {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    lat: CENTER_POINT_LAT,
+    lng: CENTER_POINT_LNG,
   },
-).addTo(map);
+  ZOOM_LEVEL,
+);
 
-const mainIcon=L.icon({
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const mainIcon = L.icon({
   iconUrl: './img/main-pin.svg',
-  iconSize: [36, 36],
-  iconAnchor: [18, 36],
+  iconSize: ICON_SIZE,
+  iconAnchor: ICON_ANCHOR,
 });
 
 const mainMarker = L.marker(
   {
-    lat: 35.6895,
-    lng: 139.692,
+    lat: CENTER_POINT_LAT,
+    lng: CENTER_POINT_LNG,
   },
   {
     draggable: true,
@@ -79,17 +84,17 @@ const mainMarker = L.marker(
 
 mainMarker.addTo(map);
 
-mainMarker.on('moveend', (evt)=>{
-  const coordinates=evt.target.getLatLng();
-  address.value=`${coordinates.lat} ${coordinates.lng}`;
+mainMarker.on('moveend', (evt) => {
+  const coordinates = evt.target.getLatLng();
+  address.value = `${coordinates.lat} ${coordinates.lng}`;
 });
 
 similarAds.forEach((similarAd) => {
-  const {location} = similarAd;
-  const icon=L.icon({
+  const { location } = similarAd;
+  const icon = L.icon({
     iconUrl: './img/pin.svg',
-    iconSize: [36, 36],
-    iconAnchor: [18, 36],
+    iconSize: ICON_SIZE,
+    iconAnchor: ICON_ANCHOR,
   });
 
   const marker = L.marker(
@@ -99,8 +104,8 @@ similarAds.forEach((similarAd) => {
     },
     {
       icon: icon,
-    });
+    },
+  );
 
-  marker.addTo(map).
-    bindPopup(getSimilarAd(similarAd));
+  marker.addTo(map).bindPopup(getSimilarAd(similarAd));
 });
